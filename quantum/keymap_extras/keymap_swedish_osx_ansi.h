1a9432092919cafd937eb7cbe5bc3ba8df1854a8
Carl-Fredrik Arvidson <carl-fredrik@arvidson.io>
2021-05-18 20:44:13 +0200

    Adds Swedish Mac ISO and ANSI keymaps

--- quantum/keymap_extras/keymap_swedish_osx_ansi.h
+++ quantum/keymap_extras/keymap_swedish_osx_ansi.h
@@ -22,15 +22,15 @@
 
 /*
  * ┌───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬─────┐
- * │ < │ 1 │ 2 │ 3 │ 4 │ 5 │ 6 │ 7 │ 8 │ 9 │ 0 │ + │ ´ │     │
+ * │ < │ 1 │ 2 │ 3 │ 4 │ 5 │ 6 │ 7 │ 8 │ 9 │ 0 │ + │ ´ │     │
  * ├───┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬───┤
- * │     │ Q │ W │ E │ R │ T │ Y │ U │ I │ O │ P │ Å │ ¨ │ ' │
+ * │     │ Q │ W │ E │ R │ T │ Y │ U │ I │ O │ P │ Å │ ¨ │ ' │
  * ├─────┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴───┤
- * │      │ A │ S │ D │ F │ G │ H │ J │ K │ L │ Ö │ Ä │      │
+ * │      │ A │ S │ D │ F │ G │ H │ J │ K │ L │ Ö │ Ä │      │
  * ├──────┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴──────┤
- * │        │ Z │ X │ C │ V │ B │ N │ M │ , │ . │ - │        │
+ * │        │ Z │ X │ C │ V │ B │ N │ M │ , │ . │ - │        │
  * ├─────┬──┴─┬─┴───┼───┴───┴───┴───┴───┴───┼───┴─┬─┴──┬─────┤
- * │     │    │     │                       │     │    │     │
+ * │     │    │     │                       │     │    │     │
  * └─────┴────┴─────┴───────────────────────┴─────┴────┴─────┘
  */
 // Row 1
@@ -87,23 +87,23 @@
 
 /* Shifted symbols
  * ┌───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬─────┐
- * │ > │ ! │ " │ # │ € │ % │ & │ / │ ( │ ) │ = │ ? │ ` │     │
+ * │ > │ ! │ " │ # │ € │ % │ & │ / │ ( │ ) │ = │ ? │ ` │     │
  * ├───┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬───┤
- * │     │   │   │   │   │   │   │   │   │   │   │   │ ^ │ * │
+ * │     │   │   │   │   │   │   │   │   │   │   │   │ ^ │ * │
  * ├─────┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴───┤
- * │      │   │   │   │   │   │   │   │   │   │   │   │      │
+ * │      │   │   │   │   │   │   │   │   │   │   │   │      │
  * ├──────┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴──────┤
- * │        │   │   │   │   │   │   │   │ ; │ : │ _ │        │
+ * │        │   │   │   │   │   │   │   │ ; │ : │ _ │        │
  * ├─────┬──┴─┬─┴───┼───┴───┴───┴───┴───┴───┼───┴─┬─┴──┬─────┤
- * │     │    │     │                       │     │    │     │
+ * │     │    │     │                       │     │    │     │
  * └─────┴────┴─────┴───────────────────────┴─────┴────┴─────┘
  */
 // Row 1
 #define SE_RABK S(SE_LABK) // >
-#define SE_EXLM S(SE_1)    // !
+#define SE_EXLM S(SE_1)    // ! 
 #define SE_DQUO S(SE_2)    // "
 #define SE_HASH S(SE_3)    // #
-#define SE_EURO S(SE_4)    // €
+#define SE_CURR S(SE_4)    // €
 #define SE_PERC S(SE_5)    // %
 #define SE_AMPR S(SE_6)    // &
 #define SE_SLSH S(SE_7)    // /
@@ -122,26 +122,25 @@
 
 /* Alted symbols
  * ┌───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬─────┐
- * │ ≤ │ © │ ™ │ £ │ $ │ ∞ │ § │ | │ [ │ ] │ ≈ │ ± │   │     │
+ * │ ≤ │ © │ @ │ £ │ $ │ ∞ │   │ | │ [ │ ] │ ≈ │ ± │   │     │
  * ├───┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬───┤
- * │     │ • │ Ω │ É │ ® │ † │ µ │ Ü │ ı │ Œ │ π │ ˙ │ ~ │ @ │
+ * │     │ • │ Ω │ é │ ® │ † │ µ │ ü │ ı │ œ │ π │ ˙ │ ~ │ ™ │
  * ├─────┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴───┤
- * │      │  │ ß │ ∂ │ ƒ │ ¸ │ ˛ │ √ │ ª │ ﬁ │ Ø │ Æ │      │
+ * │      │  │ ß │ ∂ │ ƒ │ ¸ │ ˛ │ √ │ ª │ ﬁ │ ø │ æ │      │
  * ├──────┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴──────┤
- * │        │ ÷ │   │ Ç │ ‹ │ › │ ‘ │ ’ │ ‚ │ … │ – │        │
+ * │        │ ÷ │   │ ç │ ‹ │ › │ ‘ │ ’ │ ‚ │ … │ – │        │
  * ├─────┬──┴─┬─┴───┼───┴───┴───┴───┴───┴───┼───┴─┬─┴──┬─────┤
- * │     │    │     │                       │     │    │     │
+ * │     │    │     │                       │     │    │     │
  * └─────┴────┴─────┴───────────────────────┴─────┴────┴─────┘
  * 
  */
 // Row 1 
 #define SE_LTEQ A(SE_LABK) // ≤
 #define SE_COPY A(SE_1)    // ©
-#define SE_TM   A(SE_2)    // ™
+#define SE_AT   A(SE_2)    // @
 #define SE_PND  A(SE_3)    // £
 #define SE_DLR  A(SE_4)    // $
 #define SE_INFN A(SE_5)    // ∞
-#define SE_SECT A(SE_6)    // §
 #define SE_PIPE A(SE_7)    // |
 #define SE_LBRC A(SE_8)    // [
 #define SE_RBRC A(SE_9)    // ]
@@ -150,17 +149,17 @@
 // Row 2
 #define SE_BULT A(SE_Q)    // •
 #define SE_OMEG A(SE_W)    // Ω
-#define SE_EACU A(SE_E)    // É
+#define SE_EACU A(SE_E)    // é
 #define SE_REGD A(SE_R)    // ®
 #define SE_DAGG A(SE_T)    // †
 #define SE_MICR A(SE_Y)    // µ
-#define SE_UDIA A(SE_U)    // Ü
+#define SE_UDIA A(SE_U)    // ü
 #define SE_DLSI A(SE_I)    // ı
-#define SE_OE   A(SE_O)    // Œ
+#define SE_OE   A(SE_O)    // œ
 #define SE_PI   A(SE_P)    // π
 #define SE_DOTA A(SE_ARNG) // ˙
 #define SE_TILD A(SE_DIAE) // ~ (dead)
-#define SE_AT   A(SE_QUOT) // @
+#define SE_TM   A(SE_QUOT) // ™
 // Row 3
 #define SE_APPL A(SE_A)    //  (Apple logo)
 #define SE_SS   A(SE_S)    // ß
@@ -171,11 +170,11 @@
 #define SE_SQRT A(SE_J)    // √
 #define SE_FORD A(SE_K)    // ª
 #define SE_FI   A(SE_L)    // ﬁ
-#define SE_OSTR A(SE_ODIA) // Ø
-#define SE_AE   A(SE_ADIA) // Æ
+#define SE_OSTR A(SE_ODIA) // ø
+#define SE_AE   A(SE_ADIA) // æ
 // Row 4
 #define SE_DIV  A(SE_Z)    // ÷
-#define SE_CCED A(SE_C)    // Ç
+#define SE_CCCE A(SE_C)    // ç
 #define SE_LSAQ A(SE_V)    // ‹
 #define SE_RSAQ A(SE_B)    // ›
 #define SE_LSQU A(SE_N)    // ‘
@@ -186,15 +185,15 @@
 
 /* Shift+Alted symbols
  * ┌───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬─────┐
- * │ ≥ │ ¡ │   │ ¥ │ ¢ │ ‰ │ ¶ │ \ │ { │ } │ ≠ │ ¿ │   │     │
+ * │ ≥ │ ¡ │   │ ¥ │ ¢ │ ‰ │   │ \ │ { │ } │ ≠ │ ¿ │   │     │
  * ├───┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬───┤
- * │     │ ° │ ˝ │   │   │ ‡ │ ˜ │   │ ˆ │   │ ∏ │ ˚ │   │   │
+ * │     │   │   │   │   │ ‡ │   │   │   │   │   │ ˚ │   │   │
  * ├─────┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴───┤
- * │      │ ◊ │ ∑ │ ∆ │ ∫ │ ¯ │ ˘ │ ¬ │ º │ ﬂ │   │   │      │
+ * │      │ ◊ │ ∑ │ ∆ │ ∫ │ ¯ │ ˘ │ ¬ │ º │   │   │   │      │
  * ├──────┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴──────┤
- * │        │ ⁄ │ ˇ │   │ « │ » │ “ │ ” │ „ │ · │ — │        │
+ * │        │ ⁄ │ ˇ │   │ « │ » │ “ │ ” │ „ │ · │ — │        │
  * ├─────┬──┴─┬─┴───┼───┴───┴───┴───┴───┴───┼───┴─┬─┴──┬─────┤
- * │     │    │     │                       │     │    │     │
+ * │     │    │     │                       │     │    │     │
  * └─────┴────┴─────┴───────────────────────┴─────┴────┴─────┘
  * 
  */
@@ -204,19 +203,13 @@
 #define SE_YEN  S(A(SE_3))    // ¥
 #define SE_CENT S(A(SE_4))    // ¢
 #define SE_PERM S(A(SE_5))    // ‰
-#define SE_PILC S(A(SE_6))    // ¶
 #define SE_BSLS S(A(SE_7))    // (backslash)
 #define SE_LCBR S(A(SE_8))    // {
 #define SE_RCBR S(A(SE_9))    // }
 #define SE_NEQL S(A(SE_0))    // ≠
 #define SE_IQUE S(A(SE_PLUS)) // ¿
 // Row 2
-#define SE_DEG  S(A(SE_Q))    // °
-#define SE_DACU S(A(SE_W))    // ˝
 #define SE_DDAG S(A(SE_T))    // ‡
-#define SE_STIL S(A(SE_Y))    // ˜
-#define SE_DCIR S(A(SE_I))    // ˆ
-#define SE_NARP S(A(SE_P))    // ∏
 #define SE_RNGA S(A(SE_ARNG)) // ˚
 // Row 3
 #define SE_LOZN S(A(SE_A))    // ◊
@@ -227,7 +220,6 @@
 #define SE_BREV S(A(SE_H))    // ˘
 #define SE_NOT  S(A(SE_J))    // ¬
 #define SE_MORD S(A(SE_K))    // º
-#define SE_FL   S(A(SE_L))    // ﬂ
 // Row 4
 #define SE_FRSL S(A(SE_Z))    // ⁄
 #define SE_CARN S(A(SE_X))    // ˇ
@@ -238,3 +230,4 @@
 #define SE_DLQU S(A(SE_COMM)) // „
 #define SE_MDDT S(A(SE_DOT))  // ·
 #define SE_MDSH S(A(SE_MINS)) // —
+
