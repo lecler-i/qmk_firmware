7f038e7c52dbe856c0b4aec460bf4e2bfe2e6fa1
Carl-Fredrik Arvidson <carl-fredrik@arvidson.io>
2021-06-14 08:13:09 +0200

    fix the thumb

--- keyboards/handwired/tbkmini/keymaps/cfarvidson/keymap.c
+++ keyboards/handwired/tbkmini/keymaps/cfarvidson/keymap.c
@@ -89,7 +89,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                         KC_N,    KC_M, SE_COMM , SE_DOT,SE_MINS , KC_LSFT,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-            KC_B,LT(_WORDNAV,KC_BSPC), LM(_APPS,MOD_LGUI),    TOP_ROW, KC_ENT , LT(_MOUSE,KC_SPC)
+            LCTL_T(KC_TAB),LT(_WORDNAV,KC_BSPC), LM(_APPS,MOD_LGUI),    TOP_ROW, KC_ENT , LT(_MOUSE,KC_SPC)
                                       //`--------------------------'  `--------------------------'
 
   ),
