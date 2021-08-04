6e7056d91e042bf13b4816ddc2307781c5edf2a6
Carl-Fredrik Arvidson <carl-fredrik@arvidson.io>
2021-08-04 08:25:14 +0200

    update config

--- keyboards/handwired/tbkmini/keymaps/akebanebashi/config.h
+++ keyboards/handwired/tbkmini/keymaps/akebanebashi/config.h
@@ -25,7 +25,7 @@
 #define COMBO_COUNT 5
 // Prevent normal rollover on alphas from accidentally triggering mods.
 #define IGNORE_MOD_TAP_INTERRUPT
-#define PERMISSIVE_HOLD_PER_KEY
+// #define PERMISSIVE_HOLD_PER_KEY
 
 
 // Enable rapid switch from tap to hold, disables double tap hold auto-repeat.
@@ -38,15 +38,3 @@
 #define NO_AUTO_SHIFT_ALPHA
 #define AUTO_SHIFT_TIMEOUT TAPPING_TERM
 #define AUTO_SHIFT_NO_SETUP
-
-// // Mouse key speed and acceleration.
-// #undef MOUSEKEY_DELAY
-// #define MOUSEKEY_DELAY          0
-// #undef MOUSEKEY_INTERVAL
-// #define MOUSEKEY_INTERVAL       16
-// #undef MOUSEKEY_WHEEL_DELAY
-// #define MOUSEKEY_WHEEL_DELAY    0
-// #undef MOUSEKEY_MAX_SPEED
-// #define MOUSEKEY_MAX_SPEED      7
-// #undef MOUSEKEY_TIME_TO_MAX
-// #define MOUSEKEY_TIME_TO_MAX    48
