d9cb0886a2ec379e210813eae26b4175d2dcb353
Carl-Fredrik Arvidson <carl-fredrik@arvidson.io>
2021-08-06 11:05:57 +0200

    make akebanebashi for dactyl

--- keyboards/handwired/dactyl_manuform/5x6/rules.mk
+++ keyboards/handwired/dactyl_manuform/5x6/rules.mk
@@ -20,4 +20,5 @@ RGBLIGHT_ENABLE = no       # Enable WS2812 RGB underlight.
 SLEEP_LED_ENABLE = no    # Breathing sleep LED during USB suspend
 
 COMBO_ENABLE = yes
-TAP_DANCE_ENABLE = yes
+TAP_DANCE_ENABLE = no
+AUTO_SHIFT_ENABLE = yes # Auto Shift
