d0cd33fe79265f5bfaf64e6bf023bc5a292c4024
Carl-Fredrik Arvidson <carl-fredrik@arvidson.io>
2021-08-11 11:09:22 +0200

    Bring back PERMISSIVE_HOLD_PER_KEY

--- keyboards/handwired/tbkmini/keymaps/akebanebashi/config.h
+++ keyboards/handwired/tbkmini/keymaps/akebanebashi/config.h
@@ -25,7 +25,7 @@
 #define COMBO_COUNT 5
 // Prevent normal rollover on alphas from accidentally triggering mods.
 #define IGNORE_MOD_TAP_INTERRUPT
-// #define PERMISSIVE_HOLD_PER_KEY
+#define PERMISSIVE_HOLD_PER_KEY
 
 
 // Enable rapid switch from tap to hold, disables double tap hold auto-repeat.
